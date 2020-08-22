37c223f1461a5e67d47a68fc5c3cead6cdf66ed6
OliverRubi29 <oliver.rubi13@gmail.com>
2020-08-22 22:15:28 +0800

    yaml and docker compose

--- src/main/java/com/kuwago/demo/repository/NoteRepository.java
+++ src/main/java/com/kuwago/demo/repository/NoteRepository.java
@@ -2,9 +2,11 @@ package com.kuwago.demo.repository;
 
 import com.kuwago.demo.model.Note;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface NoteRepository extends CassandraRepository<Note, UUID> {
 
 }
---
+++ src/main/java/com/kuwago/demo/service/NoteService.java
@@ -0,0 +1,4 @@
+package com.kuwago.demo.service;
+
+public interface NoteService {
+}
